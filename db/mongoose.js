d3517c63ad8f18fc78ad8bcaeef3cf4868659c84
mnali220594 <muhammadnurali303@gmail.com>
2019-11-26 13:32:43 +0700

    Add cors

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,8 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-})
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 require("./db/mongoose");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT;
+
+app.use(cors());
 
 app.use(express.json());
 app.use("/user", userRouter);
